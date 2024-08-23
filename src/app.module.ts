e304c4e1b22bbb201b05e3cab9710a78fbd0d7bc
Arthur Coelho <arthurlevicoelho@hotmail.com>
2024-08-23 09:55:32 -0300

    feat: first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { CoursesModule } from './courses/courses.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    isGlobal:true
+  }),
+    CoursesModule,
+    DatabaseModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
